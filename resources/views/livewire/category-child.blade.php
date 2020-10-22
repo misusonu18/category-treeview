28e4bc79ac982d5cf8f0dc16f6c5c0c26e89aafc
misusonu18 <mishal@freshbits.in>
2020-10-22 22:01:02 +0530

    code Manage Children blade file

---
+++ resources/views/livewire/category-child.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <ul class="list-group list-group-flush">
+        @foreach($children as $child)
+            <li class="cursor-pointer list-group-item m-2">
+                {{ $child->title }}
+                <button type="button" wire:click="$emit('addChildCategory', {{ $child->id }})" class="btn btn-primary btn-sm">
+                    <i class="fa fa-plus"></i>
+                </button>
+
+                <button wire:click="$emit('editCategory', {{ $child->id }})" class="btn btn-warning btn-sm">
+                    <i class="fa fa-edit"></i>
+                </button>
+
+                <button type="button" wire:click="$emit('deleteCategory', {{ $child->id }})" class="btn btn-danger btn-sm">
+                    <i class="fa fa-trash"></i>
+                </button>
+
+                @if(count($child->child))
+                    @livewire('category-child', ['children' => $child->child], key($child->id))
+                @endif
+            </li>
+        @endforeach
+    </ul>
+</div>
