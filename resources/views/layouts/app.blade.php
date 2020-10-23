f5c7da4e8b9315505be17f5f02632d7c626c26a8
misusonu18 <mishal@freshbits.in>
2020-10-23 17:33:29 +0530

    code: added bootstrap javascript link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
         {{ $slot }}
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     @livewireScripts
     @yield('footer')
 </body>
