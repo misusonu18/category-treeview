23947d726db679a0adaa469afba39703d395e316
misusonu18 <mishal@freshbits.in>
2020-11-09 17:16:43 +0530

    added bootstrap css and select box

--- resources/views/livewire/category.blade.php
+++ resources/views/livewire/category.blade.php
@@ -5,16 +5,39 @@
 
     <hr class="bg-success">
 
+    <div id="categorySelection">
+        <select class="custom-select" id="category" onchange="myCategory('category')">
+            <option value="0"> Select Choice </option>
+            @foreach($categories as $category)
+                <option value="{{ $category->id }}"> {{ $category->title }} </option>
+            @endforeach
+        </select>
+    </div>
+
+
+    <hr class="bg-success">
+
+     <div class="row justify-content-end">
+        <div class="col-8 pb-0">
+            @if (session()->has('message'))
+                <div class="alert alert-danger alert-dismissible fade show">
+                    <button type="button" class="close" data-dismiss="alert">&times;</button>
+                    <strong>{{ session('message') }}</strong>
+                </div>
+            @endif
+        </div>
+    </div>
+
     <div class="row justify-content-center">
         <div class="col-6">
             <button wire:click="addCategory" class="btn btn-primary">Add Category</button>
 
-            <ul id="tree" class="list-group list-group-flush m-1">
+            <ul id="tree" class="list-group list-group-flush">
                 @foreach($categories as $category)
-                    <li class="cursor-pointer list-group-item">
+                    <li class="list-group-item">
                         {{ $category->title }}
                         <button type="button"
-                            wire:click="$emit('addChildCategory', {{ $category->id }})"
+                            wire:click="$emit('getChildCategory', {{ $category->id }})"
                             class="btn btn-primary btn-sm"
                         >
                             <i class="fa fa-plus"></i>
@@ -29,13 +52,16 @@
 
                         <button type="button"
                             wire:click="$emit('deleteCategory',{{ $category->id }})"
+                            onclick="
+                                confirm('Are you sure you want to delete this category and all of its subcategories?') || event.stopImmediatePropagation()
+                            "
                             class="btn btn-danger btn-sm"
                         >
                             <i class="fa fa-trash"></i>
                         </button>
 
                         @if(count($category->child))
-                            @livewire('category-child', ['children' => $category->child], key($category->id))
+                            <livewire:category-child :children="$category->child" :key="$category->id">
                         @endif
                     </li>
                 @endforeach
@@ -49,6 +75,7 @@
                         <label>Update Category Name:</label>
                         <input type="text" class="form-control" wire:model="title">
                     </div>
+                    @error('title') <span class="error">{{ $message }}</span> @enderror
 
                     <div class="form-group">
                         <input type="submit" class="btn btn-warning btn-block rounded"
@@ -59,11 +86,12 @@
             @endif
 
             @if($createCategory)
-                <form wire:submit.prevent="{{ $categoryId ? "addChildCategory( $categoryId )" : "addCategory" }}">
+                <form wire:submit.prevent="{{ $categoryId ? "addChildCategory($categoryId)" : "addCategory" }}">
                     <div class="form-group">
                         <label>{{ $categoryId ? "Add Child Category" : "Add Category" }}</label>
                         <input type="text" class="form-control" wire:model="title">
                     </div>
+                    @error('title') <span class="error">{{ $message }}</span> @enderror
 
                     <div class="form-group">
                         <input type="submit" class="btn btn-primary btn-block rounded" value="Add">
@@ -73,3 +101,8 @@
         </div>
     </div>
 </div>
+@section('footer')
+<script>
+
+</script>
+@endsection
