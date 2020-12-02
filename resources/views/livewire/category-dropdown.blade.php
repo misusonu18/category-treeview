8e06541ac2946aae1a674a17b0757dcd28171395
misusonu18 <mishal@freshbits.in>
2020-12-02 20:02:02 +0530

    blade file of category dropdown

---
+++ resources/views/livewire/category-dropdown.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="row justify-content-center">
+        <h1>Tree View</h1>
+    </div>
+
+    <hr class="bg-success">
+
+    <div id="categorySelection">
+        <select class="custom-select" name="category" id="category" onchange="myCategory('category')">
+            <option value="0"> Select Choice </option>
+            @foreach($categories as $category)
+                <option value="{{ $category->id }}" data-id="{{ $category->title }}"> {{ $category->title }} </option>
+            @endforeach
+        </select>
+    </div>
+
+    <hr class="bg-success">
+</div>
