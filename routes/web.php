62a17523ee1b2f4d1ebaf2e3360ecf66920f00d9
misusonu18 <mishal@freshbits.in>
2020-12-02 20:02:14 +0530

    code: added route file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Livewire\Category;
+use App\Http\Livewire\CategoryDropdown;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Category::class);
+Route::get('/category', Category::class);
+Route::get('/', CategoryDropdown::class);
